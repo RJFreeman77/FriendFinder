81fbab8eb74744321189cf8534f6939eec20431e
RyanJF <ryanjfreeman77@gmail.com>
2018-11-07 21:26:07 -0600

    can post data.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,11 @@
 const userData = require("../data/users.js");
 
 module.exports = function (app) {
-    app.post("api/users", (req, res) => {
-        console.log(req.body.userName);
+    app.post("/api/users", (req, res) => {
+        // console.log(req.body);
         userData.push(req.body);
-        
+        console.log(userData);
+
         res.json(userData);
     });
 
