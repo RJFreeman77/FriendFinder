42587bbb8442afa34ff0f2af8be498e2d7490c3a
RyanJF <ryanjfreeman77@gmail.com>
2018-11-11 00:16:13 -0600

    added dummy users

--- app/data/users.js
+++ app/data/users.js
@@ -6,8 +6,18 @@ let userArry = [
         scoreArry: [1, 2, 5, 3, 4, 1, 3, 2, 1, 4]
     }, {
         userName: "Ice King",
-        twitter: "@IceKingdom",
+        twitter: "@IceKingdomRulz",
         photoLink: "https://upload.wikimedia.org/wikipedia/en/7/78/Ice_King.png",
+        scoreArry: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    }, {
+        userName: "Princess Bubblegum",
+        twitter: "OnTopOfIt",
+        photoLink: "https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/0/00/Princess_Bubblegum.png/revision/latest?cb=20120921151236&format=original",
+        scoreArry: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    }, {
+        userName: "Jake",
+        twitter: "@stretchGoals",
+        photoLink: "https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/3/3b/Jakesalad.png/revision/latest?cb=20160503014517&format=original",
         scoreArry: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
     }
 ];
